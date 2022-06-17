9915a63819e1c47e0618c6be3d14e8b71a2d8974
Brian Haas <brianjohnhaas@gmail.com>
2022-06-17 11:35:24 -0400

    added sample set generation

--- terra_job_info.py
+++ terra_job_info.py
@@ -24,19 +24,25 @@ def main():
 
     parser.add_argument("--job_id", "-j", dest="job_id", type=str, required=False, help="provide details for job/submission id")
 
+    parser.add_argument("--failures_to_sampleset", type=str, required=False, default=None, help="make a sampleset from the failed jobs")
+    
 
     args = parser.parse_args()
 
 
     project = args.project
     workspace = args.workspace
+    samplesetname = args.failures_to_sampleset
 
     if args.job_id:
 
         response = fiss.fapi.get_submission(project, workspace, args.job_id).json()
         df = pd.DataFrame(response["workflows"])
-        print(df.to_csv(sep="\t"))
-
+        if samplesetname:
+            write_failure_sampleset(df, samplesetname)
+        else:
+            print(df.to_csv(sep="\t"))
+            
         
     else:
         
@@ -46,6 +52,22 @@ def main():
 
 
     sys.exit(0)
+
+
+
+def write_failure_sampleset(df, samplesetname):
+
+    print("\t".join(["membership:sample_set_id", "sample"]))
+    
+    df = df[ df['status'] != 'Succeeded' ]
+    if len(df) > 0:
+        for _, row in df.iterrows():
+            jsinfo = row['workflowEntity']
+            samplename = jsinfo['entityName']
+            print("\t".join([samplesetname, samplename]))
+        
+    return
+
 
 
 if __name__=='__main__':
